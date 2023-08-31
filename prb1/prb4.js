b1ed3c603df582bb543fc43ddb1731814a40cc90
unknown <lalwadiyashubham123@gmail.com>
2023-08-31 22:51:14 +0530

    done

---
+++ prb1/prb4.js
@@ -0,0 +1,36 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+
+
+
+console.log("       ");
+console.log("       ");
+console.log("       ");
+console.log("🥇🥇🥇🥇🥇🥇🥇🥇🎯RESULT🎯🥇🥇🥇🥇🥇🥇🥇🥇");
+
+var name = "Rencho";
+var school = "ladakh primary school";
+var grade = "A";
+var section = "A";
+var rollno = 1;
+var marks = 300;
+
+console.log("🎖️                                       🎖️");
+console.log("🎖️   ", "NAME -",name,"                     🎖️");
+console.log("🎖️   ","SCHOOL -",school,"    🎖️");
+console.log("🎖️   ","GRADE -",grade,"                         🎖️");
+console.log("🎖️   ","SECTION -",section,"                       🎖️");
+console.log("🎖️   ","ROLLNO. -",rollno,"                       🎖️");
+console.log("🎖️   ","MARKS -",marks,"                       🎖️");
+console.log("🎖️                                       🎖️");
+
+console.log("🎖️   ","maths","   ","science","   ", " english","    🎖️");
+console.log("🎖️   ","100","      ","100","        ", "100","       🎖️");
+
+console.log("🎖️                                       🎖️");
+console.log("🥇🥇🥇🥇🥇🥇🥇🥇🎯RESULT🎯🥇🥇🥇🥇🥇🥇🥇🥇");
+            
